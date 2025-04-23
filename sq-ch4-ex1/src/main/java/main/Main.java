754b0b8cf57ea7a1affb84766878005207cf4cae
Jozewin <josewinjosewin@gmail.com>
2025-04-23 19:04:59 +0530

    4.2 Using dependency injection with abstractions

--- sq-ch4-ex1/src/main/java/main/Main.java
+++ sq-ch4-ex1/src/main/java/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 // Using interfaces to define contracts
+import config.ProjectConfiguration;
 import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailCommentNotificationProxy;
 import repositories.CommentRepository;
 import repositories.DBCommentRepository;
@@ -8,19 +10,16 @@ import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
 
-        var commentService = new CommentService(
-                commentRepository, commentNotificationProxy
-        );
 
         var comment = new Comment();
 
         comment.setText("Hi this the text");
         comment.setAuthor("Josewin");
 
-        commentService.publicComment(comment);
+        var commonService = context.getBean(CommentService.class);
+        commonService.publicComment(comment);
     }
 }
